9ba6b804b750591d097a718efbfea56fd848bca2
paulhurrle <paul_hurrle@yahoo.com>
2017-07-22 11:07:48 -0700

    complete checkpoint5: define controllers; instantiate controllers for each state in app.js; bind to view; build albums in collection.html using ngRepeat

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,50 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }
+    ]
+};
+
+var albumTen = {
+    title: 'Ten',
+    artist: 'Pearl Jam',
+    label: 'Epic/Sony Records',
+    year: '1991',
+    albumArtUrl: 'assets/images/album_covers/Ten.jpeg',
+    songs: [
+        { title: 'Once', duration: '3:52' },
+        { title: 'Even Flow', duration: '4:54' },
+        { title: 'Alive', duration: '5:41' },
+        { title: 'Why Go', duration: '3:19' },
+        { title: 'Black', duration: '5:44' },
+        { title: 'Jeremy', duration: '5:19' },
+        { title: 'Oceans', duration: '2:42' },
+        { title: 'Porch', duration: '3:31' },
+        { title: 'Garden', duration: '4:59' },
+        { title: 'Deep', duration: '4:18' },
+        { title: 'Release', duration: '9:06' }
+    ]
+};
